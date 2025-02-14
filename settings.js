07694830595ac461fe8e53ed2cd63995fa471d4f
Nethu111 <methwannethupa111@gmail.com>
2025-02-14 23:26:50 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
-module.exports = { 
+module.exports = {  
 
 SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=3voAQK7I#VMpE5BmvO92611KLwMSSFbK8FylEPttd5h3VWmLUOl0' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
